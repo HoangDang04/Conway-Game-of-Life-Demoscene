2a8ca587e179e92adf35ece31fe8db31955d10fe
unknown <ajspyridakis@gmail.com>
2025-10-29 13:49:20 -0400

    Create VGA Playground branch for easy testing
    Signals must match their hvsync_gen.

--- src/project.v
+++ src/project.v
@@ -1,36 +1,51 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Uri Shaked
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_vga_example(
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+  input  wire       clk,      // clock
+  input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // VGA Connections
+  // VGA signals
   wire hsync;
   wire vsync;
   wire [1:0] red;
   wire [1:0] green;
   wire [1:0] blue;
+  wire video_active;
   wire [9:0] vpos;
   wire [9:0] hpos;
+  wire sound;
 
-  // Assign output based on tiny-vga pinout: https://github.com/mole99/tiny-vga
+  hvsync_generator hvsync_gen(
+  .clk(clk),
+  .reset(~rst_n),
+  .hsync(hsync),
+  .vsync(vsync),
+  .display_on(video_active),
+  .hpos(hpos),
+  .vpos(vpos)
+);
+
+  // TinyVGA PMOD
   assign uo_out = {hsync, blue[0], green[0], red[0], vsync, blue[1], green[1], red[1]};
+
+  // Unused outputs assigned to 0.
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // Suppress unused signals warning
+  wire _unused_ok = &{ena, ui_in, uio_in};
 
   localparam BitWidthTotal = 640;
   localparam BitHeightTotal = 480;
@@ -47,15 +62,6 @@ module tt_um_example (
   localparam StartHeight = (BitHeightTotal - (BitHeight * CellHeight))/2;
   localparam EndHeight = BitHeightTotal - StartHeight - 1;
 
-  vga_synchronization vga_sync (
-    .hsync(hsync),
-    .vsync(vsync),
-    .vpos(vpos),
-    .hpos(hpos),
-    .clk(clk),
-    .reset(rst_n),
-  );
-
   reg current_state [0:CellSize];
   reg previous_state [0:CellSize];
 
@@ -71,7 +77,7 @@ module tt_um_example (
   wire current_cell = current_state[cell_index];
 
   assign red   = not_visible  ? 2'b00 :
-                 is_border    ? 2'b10 :
+                 is_border    ? 2'b11 :
                  current_cell ? 2'b00 :
                                 2'b11;
   assign green = not_visible  ? 2'b00 :
@@ -79,7 +85,7 @@ module tt_um_example (
                  current_cell ? 2'b00 :
                                 2'b11;
   assign blue  = not_visible  ? 2'b00 :
-                 is_border    ? 2'b01 :
+                 is_border    ? 2'b00 :
                  current_cell ? 2'b00 :
                                 2'b11;
 
@@ -200,7 +206,6 @@ module tt_um_example (
       current_state[239] <= 1;
       current_state[253] <= 1;
       current_state[255] <= 1;
-
       test <= 1;
     end
 
@@ -308,8 +313,5 @@ module tt_um_example (
     end else
       frame_count <= frame_count + 1;
   end
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
