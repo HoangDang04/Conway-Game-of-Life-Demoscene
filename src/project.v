31a288fb40d9530186ee56c1966352b11504c1e2
unknown <ajspyridakis@gmail.com>
2025-10-30 12:15:45 -0400

    test

--- src/project.v
+++ src/project.v
@@ -27,77 +27,232 @@ module tt_um_example(
   wire [9:0] hpos;
   wire sound;
 
-    vga_sync vga_synchronize(
-        .hsync(hsync),
-        .vsync(vsync),
-        .vpos(vpos),
-        .hpos(hpos),
-        .clk(clk),
-        .reset(~rst_n)
-    );
-        
-  // TinyVGA PMOD
+  vga_synchronization vga_sync (
+    .hsync(hsync),
+    .vsync(vsync),
+    .vpos(vpos),
+    .hpos(hpos),
+    .clk(clk),
+    .reset(rst_n)
+  );
+
+  // Assign output based on tiny-vga pinout: https://github.com/mole99/tiny-vga
   assign uo_out = {hsync, blue[0], green[0], red[0], vsync, blue[1], green[1], red[1]};
 
-  // Unused outputs assigned to 0.
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // =============REGISTER SIZE OF THE BOARD=================//
+  localparam WIDTH = 3, HEIGHT = 3;
 
-  // Suppress unused signals warning
-  wire _unused_ok = &{ena, ui_in, uio_in};
+  localparam BOARD_WIDTH = 2 ** WIDTH;
+  localparam BOARD_HEIGHT = 2 ** HEIGHT;
+  localparam SIZE = BOARD_WIDTH * BOARD_HEIGHT;
 
+  localparam CELL_SIZE = 48;
+  localparam BACKGROUND_HEIGHT = 480 - (CELL_SIZE * BOARD_HEIGHT);     // how much left of height in the background
+  localparam BACKGROUND_WIDTH = 640 - (CELL_SIZE * BOARD_WIDTH);    // how much left of width in the background
 
-  localparam BitWidthTotal = 640;
-  localparam BitHeightTotal = 480;
+  reg curr_board [0:SIZE-1];
+  reg prev_board [0:SIZE-1];
 
-  localparam CellWidth = 16;
-  localparam CellHeight = 16;
-  localparam CellSize = 16*16 - 1;
-  localparam BitWidth = 24;
-  localparam BitHeight = 24;
-
-  localparam StartWidth = (BitWidthTotal - (BitWidth * CellWidth))/2;
-  localparam EndWidth = BitWidthTotal - StartWidth - 1;
-
-  localparam StartHeight = (BitHeightTotal - (BitHeight * CellHeight))/2;
-  localparam EndHeight = BitHeightTotal - StartHeight - 1;
-
-  reg current_state [0:CellSize];
-
-  wire is_border = (hpos < StartWidth || hpos > EndWidth || vpos < StartHeight || vpos > EndHeight) ? 1 : 0;
-
-  wire not_visible = (hpos >= BitWidthTotal || vpos >= BitHeightTotal) ? 1 : 0;
-
-  wire [3:0] cell_x = (hpos - StartWidth) / BitHeight;
-  wire [3:0] cell_y = (vpos - StartHeight) / BitWidth;
-
-  wire [7:0] cell_index = (cell_y * CellWidth) + cell_x;
-
-  wire current_cell = current_state[cell_index];
-
-  assign red   = not_visible  ? 2'b00 :
-                 is_border    ? 2'b11 :
-                 current_cell ? 2'b00 :
-                                2'b11;
-  assign green = not_visible  ? 2'b00 :
-                 is_border    ? 2'b01 :
-                 current_cell ? 2'b00 :
-                                2'b11;
-  assign blue  = not_visible  ? 2'b00 :
-                 is_border    ? 2'b01 :
-                 current_cell ? 2'b00 :
-                                2'b11;
-
-reg [9:0] frame_count;
-
-always @(posedge vsync) begin
-  if (frame_count == 60) begin
-    current_state[1] <= ~current_state[1];
-    frame_count <= 0;
-  end else begin
-    frame_count <= frame_count + 1;
+  //==================OTHER WIRE REGISTERS=====================//                                        
+  //Checking the region belongs to 640 x 480 board or not, boundary works as a boolean
+  wire boundary = (hpos < 640 - BACKGROUND_WIDTH/2) &&
+                  (hpos >= BACKGROUND_WIDTH/2) &&
+                  (vpos < 480 - BACKGROUND_HEIGHT/2) &&
+                  (vpos >= BACKGROUND_HEIGHT/2);
+  wire visible = (hpos < 640) && (vpos < 480);
+  // Assign which cell this pixel belongs to
+  wire [WIDTH - 1: 0] row_index;
+  wire [HEIGHT - 1: 0] column_index;
+  assign row_index = (hpos - BACKGROUND_WIDTH/2) / CELL_SIZE;
+  assign column_index = (vpos - BACKGROUND_HEIGHT/2) / CELL_SIZE;
+  // Compute wheres the location of the cell in the board
+  wire [WIDTH + HEIGHT - 1: 0] location = (column_index * BOARD_WIDTH) + row_index;
+  //Genrate RGB signals for the board
+  reg [1:0] R_reg, G_reg, B_reg;
+  always @* begin
+      // Default color
+      R_reg = 2'b00;
+      G_reg = 2'b00; 
+      B_reg = 2'b00;
+      if (show_on_vga && boundary) begin
+          if (curr_board[location] == 1'b1) begin
+              R_reg = 2'b00;
+              G_reg = 2'b00;
+              B_reg = 2'b00;
+          end
+          else begin
+            R_reg = 2'b10;
+            G_reg = 2'b10;
+            B_reg= 2'b10;
+          end
+      end
+      else if (show_on_vga && visible) begin
+        R_reg = 2'b01;
+        G_reg = 2'b10;
+        B_reg = 2'b10;
+      end
   end
-end
+  assign R = R_reg;
+  assign G = G_reg;
+  assign B = B_reg;
 
-  
+  reg [5:0] frame_count;
+  integer i;
+  reg [3:0] neighbours;
+
+  reg [1:0] test;
+//======================= LOGIC ================================//
+  always @(posedge reset) begin
+    // set initial state
+  for(i = 0; i <= (SIZE - 1); i++)
+      curr_board[i] = 0;
+      curr_board[3] <= 1;
+      curr_board[6] <= 1;
+      curr_board[19] <= 1;
+      curr_board[22] <= 1;
+      curr_board[35] <= 1;
+      curr_board[38] <= 1;
+      curr_board[52] <= 1;
+      curr_board[53] <= 1;
+
+      // W
+      curr_board[8] <= 1;
+      curr_board[12] <= 1;
+      curr_board[24] <= 1;
+      curr_board[28] <= 1;
+      curr_board[40] <= 1;
+      curr_board[42] <= 1;
+      curr_board[44] <= 1;
+      curr_board[57] <= 1;
+      curr_board[59] <= 1;
+
+      // // E(1)
+      // curr_board[82] <= 1;
+      // curr_board[83] <= 1;
+      // curr_board[84] <= 1;
+      // curr_board[98] <= 1;
+      // curr_board[114] <= 1;
+      // curr_board[115] <= 1;
+      // curr_board[130] <= 1;
+      // curr_board[146] <= 1;
+      // curr_board[147] <= 1;
+      // curr_board[148] <= 1;
+
+      // // C
+      // curr_board[87] <= 1;
+      // curr_board[88] <= 1;
+      // curr_board[103] <= 1;
+      // curr_board[119] <= 1;
+      // curr_board[135] <= 1;
+      // curr_board[151] <= 1;
+      // curr_board[152] <= 1;
+
+      // // E(2)
+      // curr_board[91] <= 1;
+      // curr_board[92] <= 1;
+      // curr_board[93] <= 1;
+      // curr_board[107] <= 1;
+      // curr_board[123] <= 1;
+      // curr_board[124] <= 1;
+      // curr_board[139] <= 1;
+      // curr_board[155] <= 1;
+      // curr_board[156] <= 1;
+      // curr_board[157] <= 1;
+
+      // // 2
+      // curr_board[176] <= 1;
+      // curr_board[177] <= 1;
+      // curr_board[178] <= 1;
+      // curr_board[194] <= 1;
+      // curr_board[208] <= 1;
+      // curr_board[209] <= 1;
+      // curr_board[210] <= 1;
+      // curr_board[224] <= 1;
+      // curr_board[240] <= 1;
+      // curr_board[241] <= 1;
+      // curr_board[242] <= 1;
+
+      // // 9
+      // curr_board[180] <= 1;
+      // curr_board[181] <= 1;
+      // curr_board[182] <= 1;
+      // curr_board[196] <= 1;
+      // curr_board[198] <= 1;
+      // curr_board[212] <= 1;
+      // curr_board[213] <= 1;
+      // curr_board[214] <= 1;
+      // curr_board[230] <= 1;
+      // curr_board[246] <= 1;
+
+      // // 8
+      // curr_board[184] <= 1;
+      // curr_board[185] <= 1;
+      // curr_board[186] <= 1;
+      // curr_board[200] <= 1;
+      // curr_board[202] <= 1;
+      // curr_board[216] <= 1;
+      // curr_board[217] <= 1;
+      // curr_board[218] <= 1;
+      // curr_board[232] <= 1;
+      // curr_board[234] <= 1;
+      // curr_board[248] <= 1;
+      // curr_board[249] <= 1;
+      // curr_board[250] <= 1;
+
+      // // A
+      // curr_board[189] <= 1;
+      // curr_board[190] <= 1;
+      // curr_board[191] <= 1;
+      // curr_board[205] <= 1;
+      // curr_board[207] <= 1;
+      // curr_board[221] <= 1;
+      // curr_board[222] <= 1;
+      // curr_board[223] <= 1;
+      // curr_board[237] <= 1;
+      // curr_board[239] <= 1;
+      // curr_board[253] <= 1;
+      // curr_board[255] <= 1;
+
+      test <= 1;
+  end
+  always @(posedge vsync) begin
+  if(test == 1 && run == 1) begin
+    if (frame_count == 1) begin
+      for (i = 0; i <= (SIZE - 1); i++) 
+        prev_board[i] = curr_board[i];
+      for (i = 0; i <= (SIZE - 1); i++) begin
+        neighbours = 0;
+        if (i > 7 && i % 8 != 0 && prev_board[i - 8 - 1] == 1)
+          neighbours = neighbours + 1;
+        if (i > 7 && prev_board[i - 8] == 1)
+          neighbours = neighbours + 1;
+        if (i > 7 && (i + 1) % 8 != 0 && prev_board[i - 8 + 1] == 1)
+          neighbours = neighbours + 1;
+        if (i % 8 != 0 && prev_board[i - 1] == 1)
+          neighbours = neighbours + 1;
+        if ((i + 1) % 8 != 0 && prev_board[i + 1] == 1)
+          neighbours = neighbours + 1;
+        if (i < 56 && i % 8 != 0 && prev_board[i + 8 - 1] == 1)
+          neighbours = neighbours + 1;
+        if (i < 56 && prev_board[i + 8] == 1)
+          neighbours = neighbours + 1;
+        if (i < 56 && (i + 1) % 8 != 0 && prev_board[i + 8 + 1] == 1)
+          neighbours = neighbours + 1;
+        if (prev_board[i] == 1) begin
+          if(neighbours == 2 || neighbours == 3)
+            curr_board[i] = 1;
+          else
+            curr_board[i] = 0;
+        end else begin
+          if(neighbours == 3)
+            curr_board[i] = 1;
+          else
+            curr_board[i] = 0;
+        end
+      end
+      frame_count <= 0;
+    end else
+      frame_count <= frame_count + 1;
+  end
+  end
 endmodule
