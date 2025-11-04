f0c56e003fa6aefd34601b00a29e791d6d2b04ce
unknown <ajspyridakis@gmail.com>
2025-11-04 00:19:11 -0500

    tes

--- src/project.v
+++ src/project.v
@@ -42,9 +42,7 @@ module tt_um_example (
         .reset(~rst_n)
     );
     // =============REGISTER SIZE OF THE BOARD=================//
-    localparam CLOCK_FREQ = 24000000;
     localparam WIDTH = 3, HEIGHT = 3;
-    localparam interval = CLOCK_FREQ / 10;
     
     localparam BOARD_WIDTH = 2 ** WIDTH;
     localparam BOARD_HEIGHT = 2 ** HEIGHT;
@@ -108,115 +106,9 @@ module tt_um_example (
     reg [1:0] test;
 //======================= LOGIC ================================//
 always @(posedge vsync) begin
-        // set initial state
+    // set initial state
     if (frame_count == 0 && test == 0) begin
-      // U
       curr_board[3] <= 1;
-      // curr_board[6] <= 1;
-      // curr_board[19] <= 1;
-      // curr_board[22] <= 1;
-      // curr_board[35] <= 1;
-      // curr_board[38] <= 1;
-      // curr_board[52] <= 1;
-      // curr_board[53] <= 1;
-
-      // // W
-      // curr_board[8] <= 1;
-      // curr_board[12] <= 1;
-      // curr_board[24] <= 1;
-      // curr_board[28] <= 1;
-      // curr_board[40] <= 1;
-      // curr_board[42] <= 1;
-      // curr_board[44] <= 1;
-      // curr_board[57] <= 1;
-      // curr_board[59] <= 1;
-
-      // // E(1)
-      // curr_board[82] <= 1;
-      // curr_board[83] <= 1;
-      // curr_board[84] <= 1;
-      // curr_board[98] <= 1;
-      // curr_board[114] <= 1;
-      // curr_board[115] <= 1;
-      // curr_board[130] <= 1;
-      // curr_board[146] <= 1;
-      // curr_board[147] <= 1;
-      // curr_board[148] <= 1;
-
-      // // C
-      // curr_board[87] <= 1;
-      // curr_board[88] <= 1;
-      // curr_board[103] <= 1;
-      // curr_board[119] <= 1;
-      // curr_board[135] <= 1;
-      // curr_board[151] <= 1;
-      // curr_board[152] <= 1;
-
-      // // E(2)
-      // curr_board[91] <= 1;
-      // curr_board[92] <= 1;
-      // curr_board[93] <= 1;
-      // curr_board[107] <= 1;
-      // curr_board[123] <= 1;
-      // curr_board[124] <= 1;
-      // curr_board[139] <= 1;
-      // curr_board[155] <= 1;
-      // curr_board[156] <= 1;
-      // curr_board[157] <= 1;
-
-      // // 2
-      // curr_board[176] <= 1;
-      // curr_board[177] <= 1;
-      // curr_board[178] <= 1;
-      // curr_board[194] <= 1;
-      // curr_board[208] <= 1;
-      // curr_board[209] <= 1;
-      // curr_board[210] <= 1;
-      // curr_board[224] <= 1;
-      // curr_board[240] <= 1;
-      // curr_board[241] <= 1;
-      // curr_board[242] <= 1;
-
-      // // 9
-      // curr_board[180] <= 1;
-      // curr_board[181] <= 1;
-      // curr_board[182] <= 1;
-      // curr_board[196] <= 1;
-      // curr_board[198] <= 1;
-      // curr_board[212] <= 1;
-      // curr_board[213] <= 1;
-      // curr_board[214] <= 1;
-      // curr_board[230] <= 1;
-      // curr_board[246] <= 1;
-
-      // // 8
-      // curr_board[184] <= 1;
-      // curr_board[185] <= 1;
-      // curr_board[186] <= 1;
-      // curr_board[200] <= 1;
-      // curr_board[202] <= 1;
-      // curr_board[216] <= 1;
-      // curr_board[217] <= 1;
-      // curr_board[218] <= 1;
-      // curr_board[232] <= 1;
-      // curr_board[234] <= 1;
-      // curr_board[248] <= 1;
-      // curr_board[249] <= 1;
-      // curr_board[250] <= 1;
-
-      // // A
-      // curr_board[189] <= 1;
-      // curr_board[190] <= 1;
-      // curr_board[191] <= 1;
-      // curr_board[205] <= 1;
-      // curr_board[207] <= 1;
-      // curr_board[221] <= 1;
-      // curr_board[222] <= 1;
-      // curr_board[223] <= 1;
-      // curr_board[237] <= 1;
-      // curr_board[239] <= 1;
-      // curr_board[253] <= 1;
-      // curr_board[255] <= 1;
 
       test <= 1;
     end
@@ -226,29 +118,36 @@ always @(posedge vsync) begin
         prev_board[i] = curr_board[i];
       for (i = 0; i <= 255; i++) begin
         neighbours = 0;
-        if (i > 15 && i % 16 != 0 && prev_board[i - 16 - 1] == 1)
+        if (i > 7 && i % 8 != 0 && prev_board[i - 8 - 1] == 1)
           neighbours = neighbours + 1;
-        if (i > 15 && prev_board[i - 16] == 1)
+        if (i > 7 && prev_board[i - 8] == 1)
           neighbours = neighbours + 1;
-        if (i > 15 && (i + 1) % 16 != 0 && prev_board[i - 16 + 1] == 1)
+        if (i > 7 && (i + 1) % 8 != 0 && prev_board[i - 8 + 1] == 1)
           neighbours = neighbours + 1;
-        if (i % 16 != 0 && prev_board[i - 1] == 1)
+        if (i % 8 != 0 && prev_board[i - 1] == 1)
           neighbours = neighbours + 1;
-        if ((i + 1) % 16 != 0 && prev_board[i + 1] == 1)
+        if ((i + 1) % 8 != 0 && prev_board[i + 1] == 1)
           neighbours = neighbours + 1;
-        if (i < 240 && i % 16 != 0 && prev_board[i + 16 - 1] == 1)
+        if (i < 56 && i % 8 != 0 && prev_board[i + 8 - 1] == 1)
           neighbours = neighbours + 1;
-        if (i < 240 && prev_board[i + 16] == 1)
+        if (i < 56 && prev_board[i + 8] == 1)
           neighbours = neighbours + 1;
-        if (i < 240 && (i + 1) % 16 != 0 && prev_board[i + 16 + 1] == 1)
+        if (i < 56 && (i + 1) % 8 != 0 && prev_board[i + 8 + 1] == 1)
           neighbours = neighbours + 1;
-        if (neighbours == 2 || neighbours == 3)
-          curr_board[i] = 1;
-        else 
-         curr_board[i] = 0;
+        if (prev_board == 1) begin
+          if (neighbours == 2 || neighbours == 3)
+            curr_board[i] <= 1;
+          else 
+            curr_board[i] <= 0;
+        end else begin 
+          if (neighbours == 3)
+            curr_board[i] <= 1;
+          else 
+            curr_board[i] <= 0;
       end
       frame_count <= 0;
     end else
       frame_count <= frame_count + 1;
   end
+end
 endmodule
