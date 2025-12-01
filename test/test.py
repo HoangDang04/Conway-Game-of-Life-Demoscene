f8b4b64df22cfc21273e3cfbce1b922d0f05dadf
unknown <ajspyridakis@gmail.com>
2025-11-30 20:16:19 -0500

    test

--- test/test.py
+++ test/test.py
@@ -22,7 +22,7 @@ async def setup_test(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-    await FallingEdge(dut.clk)
+    await ClockCycles(dut.clk, 420000)
 
 @cocotb.test()
 async def vga_horizontal_test(dut):
@@ -30,6 +30,7 @@ async def vga_horizontal_test(dut):
 
     dut._log.info("Test horizontal sync")
     # Pixel generation region
+    await ClockCycles(dut.clk, 640)
     for i in range (1000) :
         dut._log.info(dut.uo_out.value)
         dut._log.info(dut.uio_out.value)
