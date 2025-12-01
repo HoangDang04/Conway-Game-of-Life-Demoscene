4a2dcf84e0d665888af6518457661b36da9193eb
unknown <ajspyridakis@gmail.com>
2025-11-30 20:08:43 -0500

    Test

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 from cocotb.triggers import ReadOnly
+from cocotb.triggers import FallingEdge
 
 async def setup_test(dut):
     dut._log.info("Start")
@@ -21,7 +22,7 @@ async def setup_test(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 800)
+    await FallingEdge(dut.clk)
 
 @cocotb.test()
 async def vga_horizontal_test(dut):
@@ -33,7 +34,7 @@ async def vga_horizontal_test(dut):
         await ReadOnly()
         dut._log.info(dut.uo_out.value)
         dut._log.info(dut.uio_out.value)
-        await ClockCycles(dut.clk, 1)
+        await FallingEdge(dut.clk, 1)
 
     # # Front porch
     # dut._log.info("front porch")
